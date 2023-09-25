60bbcfee665d4c6a490f3b42d275ea404321a5d2
reimiii <imiia75775@gmail.com>
2023-09-25 23:53:26 +0700

    RaceConditionTest

---
+++ src/main/java/franxx/code/thread/Counter.java
@@ -0,0 +1,13 @@
+package franxx.code.thread;
+
+public class Counter {
+    private Long value = 0L;
+
+    public void increment() {
+        value++;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+}
