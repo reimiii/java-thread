d79830234972c6c460f95765dc35407057efa24c
reimiii <imiia75775@gmail.com>
2023-09-30 11:50:42 +0700

    Condition

--- src/test/java/franxx/code/thread/LockTest.java
+++ src/test/java/franxx/code/thread/LockTest.java
@@ -2,6 +2,9 @@ package franxx.code.thread;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class LockTest {
     @Test
     void testLock() throws InterruptedException {
@@ -49,5 +52,58 @@ public class LockTest {
         t3.join();
 
         System.out.println(counter.getValue());
+    }
+
+    String message;
+
+    @Test
+    void conditionAll() throws InterruptedException {
+        var lock = new ReentrantLock();
+        var condition = lock.newCondition();
+
+        var t1 = new Thread(() -> {
+            try {
+                lock.lock();
+                condition.await();
+                System.out.println(message);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                lock.unlock();
+            }
+        });
+
+        var t3 = new Thread(() -> {
+            try {
+                lock.lock();
+                condition.await();
+                System.out.println(message.toUpperCase());
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                lock.unlock();
+            }
+        });
+
+        var t2 = new Thread(() -> {
+            try {
+                lock.lock();
+                Thread.sleep(1000);
+                message = "Hilmi AM";
+                condition.signalAll();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                lock.unlock();
+            }
+        });
+
+        t1.start();
+        t3.start();
+        t2.start();
+
+        t1.join();
+        t3.join();
+        t2.join();
     }
 }
